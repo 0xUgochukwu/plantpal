29ab28cf55114822c0eeb6d430941e13dc7e35e2
Ikemba <sparkzilla7@gmail.com>
2023-08-11 11:17:39 +0100

    Plantpal

---
+++ lib/main.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:plant_app/screens/cart_screen.dart';
+import 'package:plant_app/screens/home_screen.dart';
+import 'package:plant_app/screens/login_screen.dart';
+import 'package:plant_app/screens/main_screen.dart';
+import 'package:plant_app/screens/signup_screen.dart';
+
+import 'fingerprint_auth.dart';
+
+void main() {
+  // WidgetsFlutterBinding.ensureInitialized();
+  // SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual, overlays: [
+  //   SystemUiOverlay.bottom, //This line is used for showing the bottom bar
+  // ]);
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const FingerprintScreen(),
+      routes: {
+        FingerprintScreen.id: (context) => const FingerprintScreen(),
+        LoginScreen.id: (context) => const LoginScreen(),
+        SignupScreen.id: (context) => const SignupScreen(),
+        MainScreen.id: (context) => const MainScreen(),
+        HomeScreen.id: (context) => const HomeScreen(),
+        CartScreen.id: (context) => const CartScreen(),
+      },
+    );
+  }
+}
